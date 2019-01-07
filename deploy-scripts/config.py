04c0dc7f1835aa81c87b304b600c259ac28d60fb
Vlad-Adrian Moglan <vlad-adrian.moglan@students.hs-mainz.de>
2019-01-08 00:41:40 +0100

    Simplified structure. Now requires PCL 1.8.1 to be installed.

--- deploy-scripts/config.py
+++
@@ -1,4 +0,0 @@
-# Maven configuration file: change it according to the URL of your server and the id of your repository.
-
-MAVEN_URL = "https://gitlab.rlp.net/api/v4/projects/2493/packages/maven"    # server URL
-MAVEN_REPOSITORY_ID = "gitlab-maven"    # id of the maven repository
--- deploy-scripts/deploy_all.py
+++
@@ -1,7 +0,0 @@
-# Deploys all the required Maven artifacts for the pcl-java project to the maven-releases repository on the local Nexus server
-
-import deploy_pcl_java
-import deploy_pcl_java_natives
-import deploy_pcl_natives
-import deploy_pcl_java_all
-
