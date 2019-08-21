c4af160295b3919f4da06e71fa39ac04a46d5db6
Luke Hutchison <luke.hutch@gmail.com>
2019-08-21 08:28:40 -0600

    Consistency fix

--- src/main/java/com/movlad/pcl/math/Comparison.java
+++ src/main/java/com/movlad/pcl/math/Comparison.java
@@ -3,7 +3,7 @@ package com.movlad.pcl.math;
 public class Comparison {
 	
 	public static boolean areEqualFloat(float a, float b, float precision) {
-		return Math.abs(a - b) <= precision;
+		return Math.abs(a - b) < precision;
 	}
 	
 	public static boolean areEqualDouble(double a, double b, double precision) {
