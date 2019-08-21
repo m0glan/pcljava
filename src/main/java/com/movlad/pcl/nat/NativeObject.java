e84d483170b357f6a22eac3869e17b39cfc8d561
Luke Hutchison <luke.hutch@gmail.com>
2019-08-21 08:13:53 -0600

    Update Javadoc

--- src/main/java/com/movlad/pcl/nat/NativeObject.java
+++ src/main/java/com/movlad/pcl/nat/NativeObject.java
@@ -33,9 +33,15 @@ public abstract class NativeObject implements AutoCloseable {
 
 	/**
 	 * Deallocate the native object associated with this {@link NativeObject}
-	 * instance. This should be manually called before the {@link NativeObject} goes
-	 * out of scope, since finalizers are broken in Java, and registering a
-	 * {@link Cleaner} or a {@link PhantomReference} incurs memory and CPU overhead.
+	 * instance. This should be called in a try-with-resources block, or manually
+	 * called before the {@link NativeObject} goes out of scope. Failing to call
+	 * this method before this object goes out of scope will result in a resource
+	 * leak.
+	 * 
+	 * Finalizers are <a href=
+	 * "http://openjdk.5641.n7.nabble.com/Finalization-and-dead-references-another-proposal-td321420.html">broken</a>
+	 * in Java, and registering a {@link Cleaner} or a {@link PhantomReference}
+	 * incurs memory and CPU overhead.
 	 * 
 	 * Override this method with a public native dispose() method in subclasses.
 	 */
