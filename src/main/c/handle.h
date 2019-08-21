2b3995b8c1e266f1d496c97b75a2f52768cee90d
Luke Hutchison <luke.hutch@gmail.com>
2019-08-21 06:11:12 -0600

    Optimization: cache Java field id lookup

--- src/main/c/handle.h
+++ src/main/c/handle.h
@@ -1,17 +1,40 @@
 #include <jni.h>
+#include <stdio.h>
 
 #ifndef HANDLE_H
 #define HANDLE_H
 
-inline jfieldID get_handle_field(JNIEnv *env, jobject obj) 
-{
-	jclass cls = env->GetObjectClass(obj);
+// Class reference and field id are cached in static fields, so get_handle
+// and set_handle can only be used with one type parameter per source file
+// (which is the standard usage for JNI anyway -- one C++ source file
+// handles one Java class).
+static jclass cls = 0;
+static jobject cls_ref;
+static jfieldID fid;
 
-	return env->GetFieldID(cls, "handle", "J");
+inline jfieldID get_handle_field(JNIEnv *env, jobject obj)
+{
+    if (cls == 0) {
+        cls = env->GetObjectClass(obj);
+        if (cls == 0) {
+            /* TODO: report error */
+        }
+        // Hold global ref to class -- see:
+        // http://journals.ecs.soton.ac.uk/java/tutorial/native1.1/implementing/refs.html
+        cls_ref = env->NewGlobalRef(cls);
+        if (cls_ref == 0) {
+            /* TODO: report error */
+        }
+        fid = env->GetFieldID(cls, "handle", "J");
+        if (fid == 0) {
+            /* TODO: report error */
+        }
+    }
+	return fid;
 }
 
 template <typename T>
-inline T *get_handle(JNIEnv *env, jobject obj) 
+inline T *get_handle(JNIEnv *env, jobject obj)
 {
 	jlong handle = env->GetLongField(obj, get_handle_field(env, obj));
 
@@ -19,7 +42,7 @@ inline T *get_handle(JNIEnv *env, jobject obj)
 }
 
 template <typename T>
-inline void set_handle(JNIEnv *env, jobject obj, T *ptr) 
+inline void set_handle(JNIEnv *env, jobject obj, T *ptr)
 {
 	jlong handle = reinterpret_cast<jlong>(ptr);
 
