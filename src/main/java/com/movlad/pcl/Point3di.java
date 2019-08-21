304ff5606690e9755e4e4a12e2b2f458478ca1c8
Luke Hutchison <luke.hutch@gmail.com>
2019-08-21 09:20:05 -0600

    Add Point3di class for pcl::PointXYZI

---
+++ src/main/java/com/movlad/pcl/Point3di.java
@@ -0,0 +1,69 @@
+package com.movlad.pcl;
+
+import com.movlad.pcl.math.Comparison;
+import com.movlad.pcl.nat.NativeObject;
+
+/**
+ * A point structure representing Euclidean xyz coordinates with an associated
+ * intensity value.
+ * 
+ * @see <a href=
+ *      "http://docs.pointclouds.org/1.8.0/structpcl_1_1_point_x_y_z_i.html">
+ *      pcl::PointXYZI documentation </a>
+ */
+public class Point3di extends NativeObject implements Point, Cloneable {
+
+	@Override
+	protected native void alloc();
+
+	@Override
+	public native void dispose();
+
+	public final native float getX();
+
+	public native void setX(float x);
+
+	public native float getY();
+
+	public native void setY(float y);
+
+	public native float getZ();
+
+	public native void setZ(float z);
+
+	public void setCoordinates(float x, float y, float z) {
+		setX(x);
+		setY(y);
+		setZ(z);
+	}
+
+	public final native float getI();
+
+	public final native void setI(float i);
+
+	@Override
+	public Point3di clone() {
+		Point3di clone = new Point3di();
+
+		clone.setCoordinates(getX(), getY(), getZ());
+		clone.setI(getI());
+
+		return clone;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		Point3di point = (Point3di) object;
+
+		return Comparison.areEqualFloat(getX(), point.getX(), 0.1f)
+				&& Comparison.areEqualFloat(getY(), point.getY(), 0.1f)
+				&& Comparison.areEqualFloat(getZ(), point.getZ(), 0.1f)
+				&& Comparison.areEqualFloat(getI(), point.getI(), 0.1f);
+	}
+
+	@Override
+	public String toString() {
+		return "x: " + getX() + " y: " + getY() + " z: " + getZ();
+	}
+
+}
