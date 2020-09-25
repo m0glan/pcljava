aa0684ee75e2c3c329e6e2fc3c0e161c275b534b
Vlad-Adrian Moglan <vlad.moglan@gmail.com>
2020-09-25 20:48:08 +0200

    Minor comment refactoring in native code.

--- src/main/native/SharedPointerWrapper.h
+++ src/main/native/SharedPointerWrapper.h
@@ -5,13 +5,16 @@
 
 #include "handle.h"
 
-/// This class provides a compatibility bridge between JNI and \code{std::shared_ptr<T>}; this is
-/// needed in order to prevent lifecycle problems, such as a native smart pointer that expires before
-/// the Java object that references it.
+/**
+ * Using smart pointers associated with Java objects within JNI calls
+ * does not work, since the memory they point to is freed at the end
+ * of the call. This class is used for wrapping a smart pointer to
+ * ensure that the memory pointed to is kept alive until it is disposed
+ * within the Java code.
+ */
 template <typename T>
 class SharedPointerWrapper
 {
-
 private:
 	std::shared_ptr<T> pointer;
 
@@ -34,13 +37,17 @@ public:
 		this.pointer = pointer;
 	}
 
-	/// \brief{Sets the address of \code{this} wrapper as a handle for a java object within a JNI environment.}
+	/**
+	 * Sets this as the memory handle for a Java object.
+	 */
 	void instantiate(JNIEnv *env, jobject obj)
 	{
 		setHandle<SharedPointerWrapper>(env, obj, this);
 	}
 
-	/// \returns{the address of the wrapper as a \code{jlong}.}
+	/**
+	 * @return the pointer to this wrapper as a jlong.
+	 */
 	jlong instance()
 	{
 		return std::reinterpret_pointer_cast<jlong>(this);
@@ -51,28 +58,36 @@ public:
 		return pointer;
 	}
 
-	/// @param env The JNI environment.
-	/// @param javaInstance The object for which to find the corresponding wrapper.
-	// \returns{the \code{shared_ptr} associated to a Java instance within a JNI environment.}
+	/**
+	 * @param env The JNI environment.
+	 * @param javaInstance The Java object.
+	 * @return the smart pointer associated with a Java object within a JNI environment.
+	 */
 	static std::shared_ptr<T> getPointer(JNIEnv *env, jobject javaInstance)
 	{
 		return getWrapper(env, javaInstance)->getPointer();
 	}
 
-	/// @param env The JNI environment.
-	/// @param javaInstance The object for which to find the corresponding wrapper.
-	// \returns{the pointer to the wrapper associated to a Java instance within a JNI environment.}
+	/**
+	 * @param env The JNI environment.
+	 * @param javaInstance The Java object.
+	 * @return the pointer to the smart pointer wrapper associated with a Java object within a JNI environment.
+	 */
 	static SharedPointerWrapper *getWrapper(JNIEnv *env, jobject javaInstance)
 	{
 		return getHandle<SharedPointerWrapper<T>>(env, javaInstance);
 	}
 
+	/**
+	 * Frees the memory pointed to by the wrapped smart pointer.
+	 * 
+	 * @param env The JNI environment.
+	 * @param javaInstance The Java object.
+	 */
 	static void dispose(JNIEnv *env, jobject javaInstance)
 	{
 		auto wrapper = getWrapper(env, javaInstance);
-
 		delete wrapper;
 		setHandle<SharedPointerWrapper<T>>(env, javaInstance, nullptr);
 	}
-
 };
